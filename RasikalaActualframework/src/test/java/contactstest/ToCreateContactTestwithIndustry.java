4f40bfbe2dfab0f57d87555b86c4bd18898a9f1a
rasik <rasik@DESKTOP-HNCAMB3>
2025-06-26 20:36:52 +0530

    Fifth fush

---
+++ RasikalaActualframework/src/test/java/contactstest/ToCreateContactTestwithIndustry.java
@@ -0,0 +1,10 @@
+package contactstest;
+
+public class ToCreateContactTestwithIndustry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RasikalaActualframework/src/test/java/orgnizationtest/ToCreateOrganizationWithTypeTest.java
@@ -0,0 +1,10 @@
+package orgnizationtest;
+
+public class ToCreateOrganizationWithTypeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
